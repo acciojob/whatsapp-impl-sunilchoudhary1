368ce727980b996669a2077bcdc068e23ae21a68
Sunil <sunilkumar010@gmail.com>
2023-03-03 23:17:05 +0530

    try1

--- src/main/java/com/driver/WhatsappController.java
+++ src/main/java/com/driver/WhatsappController.java
@@ -27,6 +27,9 @@ public class WhatsappController {
         //If the mobile number exists in database, throw "User already exists" exception
         //Otherwise, create the user and return "SUCCESS"
 
+        if(!whatsappService.isNewUser(mobile))
+            throw new Exception("User already exists");
+
         return whatsappService.createUser(name, mobile);
     }
 
@@ -82,12 +85,12 @@ public class WhatsappController {
         return whatsappService.removeUser(user);
     }
 
-    @GetMapping("/find-messages")
-    public String findMessage(Date start, Date end, int K) throws Exception{
-        //This is a bonus problem and does not contains any marks
-        // Find the Kth latest message between start and end (excluding start and end)
-        // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
-
-        return whatsappService.findMessage(start, end, K);
-    }
+//    @GetMapping("/find-messages")
+//    public String findMessage(Date start, Date end, int K) throws Exception{
+//        //This is a bonus problem and does not contains any marks
+//        // Find the Kth latest message between start and end (excluding start and end)
+//        // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
+//
+//        return whatsappService.findMessage(start, end, K);
+//    }
 }
